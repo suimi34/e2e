53f08c39a01b44b4329c6950abfbb7195d4489f9
Susumu Ishikawa <susumu2934@gmail.com>
2019-09-07 22:19:09 +0900

    add sample code of react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
-  );
-}
+  </BrowserRouter>
+);
+
+const Home = () => (
+  <div>
+    <h2>home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>about</h2>
+  </div>
+);
 
 export default App;
